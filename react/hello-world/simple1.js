e661560a43e1ffb3ebb5fc5a244af22d10f0b368
QT-HH <hyunheal@naver.com>
2021-09-05 21:33:14 +0900

    [210905] - 리액트 (바벨, 웹팩, css in js, code splitting)

---
+++ react/hello-world/simple1.js
@@ -0,0 +1,23 @@
+function LikeButton() {
+  const [liked, setLiked] = React.useState(false);
+  const text = liked ? '좋아요 취소' : '좋아요';
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => setLiked(!liked)
+  }, " ", text, " ");
+}
+
+function Container() {
+  const [count, setCount] = React.useState(0);
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(LikeButton, null), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("span", null, "\uD604\uC7AC \uCE74\uC6B4\uD2B8: "), /*#__PURE__*/React.createElement("span", {
+    style: {
+      marginRight: 10
+    }
+  }, count), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count + 1)
+  }, "\uC99D\uAC00"), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count - 1)
+  }, "\uAC10\uC18C")));
+}
+
+const domContainer = document.getElementById('root');
+ReactDOM.render(React.createElement(Container), domContainer1);
