1531da562688de01f78eece1d7a17f83d14577a0
QT-HH <hyunheal@naver.com>
2021-09-17 10:40:31 +0900

    [210917] 리액트 공부 - webpack 설정을 위해 eject하기 전 변경사항 커밋

--- javascript/test_async_await/asyncTest02.js
+++ javascript/test_async_await/asyncTest02.js
@@ -3,6 +3,10 @@ const promiseFn2 = Promise.resolve(2)
 const promiseFn3 = Promise.resolve(3)
 const promiseFn4 = Promise.resolve(4)
 const promiseFn5 = Promise.resolve(5)
+const promiseFn6 = Promise.resolve(6)
+const promiseFn7 = Promise.resolve(7)
+const promiseFn8 = Promise.resolve(8)
+const promiseFn9 = Promise.resolve(9)
 
 // // // no await
 // // promiseFn1.then((res1) => {
@@ -25,8 +29,20 @@ const promiseFn5 = Promise.resolve(5)
 promiseFn1.then( async (res1) => {
   await promiseFn2.then((res2) => {
     promiseFn3.then((res3) => {
-      promiseFn4.then((res4) => {
-        promiseFn5.then((res5) => {
+      promiseFn4.then( async (res4) => {
+        await promiseFn5.then((res5) => {
+          promiseFn6.then((res6) => {
+            promiseFn7.then((res7) => {
+              promiseFn8.then((res8) => {
+                promiseFn9.then((res9) => {
+                  console.log("res9: ", res9)
+                })
+                console.log("res8: ", res8)
+              })
+              console.log("res7: ", res7)
+            })
+            console.log("res6: ", res6)
+          })
           console.log("res5: ", res5)
         })
         console.log("res4: ", res4)
